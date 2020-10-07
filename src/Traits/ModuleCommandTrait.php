1f911e327cc1b46d7dfbc3a239fd57d018d40b61
Dovydas Linkus <mcraft.mine412@gmail.com>
2020-10-07 21:51:40 +0100

    Ft: Extend Module Command Trait

--- src/Traits/ModuleCommandTrait.php
+++ src/Traits/ModuleCommandTrait.php
@@ -5,16 +5,45 @@ namespace Nwidart\Modules\Traits;
 trait ModuleCommandTrait
 {
     /**
+     * Get modules
+     *
+     * @return object
+     */
+    public function getModules()
+    {
+        return app('modules');
+    }
+
+    /**
+     * Get currently used module
+     *
+     * @return object
+     */
+    public function getCurrentModule()
+    {
+        return $this->getModules()->getUsedNow();
+    }
+
+    /**
+     * Get specified module
+     *
+     * @param null|string $module
+     * @return object
+     */
+    public function getModule($module = null)
+    {
+        return $this->getModules()->findOrFail(
+            $module ?: ($this->argument('module') ?: $this->getCurrentModule())
+        );
+    }
+
+    /**
      * Get the module name.
      *
      * @return string
      */
     public function getModuleName()
     {
-        $module = $this->argument('module') ?: app('modules')->getUsedNow();
-
-        $module = app('modules')->findOrFail($module);
-
-        return $module->getStudlyName();
+        return $this->getModule()->getStudlyName();
     }
 }
